ea918ebcdd49b4e0c3f9fba013c9d7fbf7aac0ae
Sulaksh M More <38561021+msulaksh@users.noreply.github.com>
2019-06-07 10:48:59 +0530

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,3 +1,11 @@
 from flask import Flask, jsonify, request, redirect, render_template
 import json
 from flask_paginate import Pagination
+
+with open('/home/administrator/Desktop/flask_ex/data_for_rest.json', 'r'
+          ) as f:
+    data = f.read()
+
+json_formatted_data = json.loads(data)
+
+length = len(json_formatted_data['people'])
